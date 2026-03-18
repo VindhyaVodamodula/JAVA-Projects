21ca7c1ae40d353286bbae8de3a4318f1d1d5f10
Vindhya Vodamodula <vindhya.a23@gmail.com>
2026-03-18 01:19:16 -0600

    Create Wonderland Adventures project structure

---
+++ wonderland-adventures/Location.java
@@ -0,0 +1,8 @@
+//Final Project
+//Title: Wonderland Adventure 🔑🌸
+//Name: Vindhya Apoorva Vodamodula
+//Student ID: 3728392
+//Course: COMP 268 - Java Programming
+//Date: October 2025
+//Athabasca University
+//Not used in the final build; kept from early prototype
